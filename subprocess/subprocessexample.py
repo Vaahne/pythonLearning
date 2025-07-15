0ffa2bb8ebd7b55ab023c68156d65fc68b692eb1
Spring -Git <vaahne@yahoo.com>
2025-07-15 12:45:46 -0600

    subprocess

---
+++ subprocess/subprocessexample.py
@@ -0,0 +1,8 @@
+import subprocess
+subprocess.run(["date"])
+subprocess.run(["sleep","2"])  # sleeps for 2 seconds
+# subprocess.run(["ls","this_file_does_not_exist"])
+
+subprocess.run(["mkdir","sample"])
+
+# changing teh permissions
